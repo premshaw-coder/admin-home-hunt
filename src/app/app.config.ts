c523ef29a6f7eb5a55cf0eafb796164df6f1fcca
Prem Ranjan Shaw <premranjanshaw@gmail.com>
2025-02-16 00:07:52 +0530

    added new folder location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient } from '@angular/common/http';
-import { MyPreset } from '../shared/primeNg-themes/primeng-themes';
+import { MyPreset } from './shared/primeNg-themes/primeng-themes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideRouter(routes),
