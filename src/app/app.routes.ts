e7caa74f8565f2735f0b799dd916e9828a4fcb53
Prem Ranjan Shaw <premranjanshaw@gmail.com>
2025-05-26 01:15:36 +0530

    refactor: remove commented-out code in app.routes.ts for cleaner codebase refactor: add async attribute to Razorpay checkout script for improved performance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,31 +24,3 @@ export const routes: Routes = [
         path: RoutesPaths.subscription, loadComponent: () => import('../features/subscription/subscription/subscription.component').then((c) => c.SubscriptionComponent)
     }
 ];
-
-/*
-
-import { Routes } from '@angular/router';
-import { authGuard } from '../features/auth/guard/auth.guard';
-import { RoutesPaths } from './shared/application-routes/app-routes';
-
-export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: RoutesPaths.login,
-        pathMatch: 'full'
-    },
-    {
-        path: RoutesPaths.auth,
-        loadChildren: () => import('../features/auth/routes/auth.routes').then(c => c.authRoutes)
-    },
-    {
-        path: RoutesPaths.createPropertyListing,
-        canActivate: [authGuard],
-        loadComponent: () => import('../features/property-listing/property-listing/property-listing.component')
-            .then(c => c.PropertyListingComponent),
-        loadChildren: () => import('../features/property-listing/routes/property-listing-routes')
-            .then(c => c.PropertyListingRoutes)
-    }
-];
-
-*/
